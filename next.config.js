5e17b233250ad770d6c06e5c75922119f461f349
roronoazor <ugoodumegwu@gmail.com>
2023-10-11 10:24:50 +0100

    updated next.config.js added eslint build disable

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
   typescript: {
     ignoreDuringBuilds: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   modularizeImports: {
     "@mui/icons-material": {
       transform: "@mui/icons-material/{{member}}",
